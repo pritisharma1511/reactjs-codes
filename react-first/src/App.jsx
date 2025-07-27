48775b8ce44aaff89091529ca42bfd34b3d56934
Your Name <pso130815@gmail.com>
2025-07-27 09:18:25 +0530

    fragment

--- react-first/src/App.jsx
+++ react-first/src/App.jsx
@@ -1,16 +1,28 @@
+import { Fragment } from "react";
 export const App = () => {
+  return (
+    <Fragment>
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+    </Fragment>
+
+  );
+};
+
+const NetflixSeries = () => {
   return (
     <div>
       <h1>Queen of tears</h1>
-       <img src="image.png" alt="Queen of tears"
-       width="40%" height="40%" />
+      <img src="image.png" alt="Queen of tears" width="40%" height="40%" />
       <h2>name:Queen of tears</h2>
       <h3>Rating :8.2</h3>
-      <p>summary:baek Hyun-woo and hong hae-in navigate a tense relationship,
+      <p>
+        summary:baek Hyun-woo and hong hae-in navigate a tense relationship,
         both at home and at work.
       </p>
     </div>
   );
 };
-
-
