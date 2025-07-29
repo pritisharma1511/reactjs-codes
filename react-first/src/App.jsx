18451b39bef6c4f336a134d584df92f1bacff62d
Your Name <pso130815@gmail.com>
2025-07-29 21:38:24 +0530

    dynamic value

--- react-first/src/App.jsx
+++ react-first/src/App.jsx
@@ -13,16 +13,53 @@ export const App = () => {
 };
 
 const NetflixSeries = () => {
-  return (
+  const name = "Queen of tears";
+  const rating = 8.2;
+  const summary = "baek Hyun-woo and hong hae-in navigate a tense relationship, both at home and at work.";
+  let age=15;
+
+  // let canWatch = "Not Available";
+  // if (age >= 18) {
+  //   canWatch = "Watch Now";
+  // }
+  const canWatch = () => {
+    if (age >= 18) return "Watch Now";
+    return "Not Available";
+  };
+
+
+
+  const returnGenre = () => {
+    const genre="romcom";
+    return genre;
+  };
+
+
+
+//   if(age<18) {
+//   return (
+//     <div>
+//       <h1>{name}</h1>
+//       <img src="image.png" alt={name} width="40%" height="40%" />
+//       <h2>Name:{name}</h2>
+//       <h3>Rating :{rating}</h3>
+//       <p>Summary: {summary}</p> 
+//       <p>Genre: {returnGenre()}</p> 
+//       <button>Not Available</button>
+//     </div>
+//   );
+// };
+return (
     <div>
-      <h1>Queen of tears</h1>
-      <img src="image.png" alt="Queen of tears" width="40%" height="40%" />
-      <h2>name:Queen of tears</h2>
-      <h3>Rating :8.2</h3>
-      <p>
-        summary:baek Hyun-woo and hong hae-in navigate a tense relationship,
-        both at home and at work.
-      </p>
+      <h1>{name}</h1>
+      <img src="image.png" alt={name} width="40%" height="40%" />
+      <h2>Name:{name}</h2>
+      <h3>Rating :{rating}</h3>
+      <p>Summary: {summary}</p> 
+      <p>Genre: {returnGenre()}</p> 
+      {/* <button>{age >= 18 ? "Watch Now" : "Not Available"}</button> */}
+      {/* <button>{canWatch}</button> */}
+       <button>{canWatch()}</button>
     </div>
   );
 };
