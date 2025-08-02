60af73be4b9488770cc372041ab6716d0d64cffb
Your Name <pso130815@gmail.com>
2025-08-02 20:11:17 +0530

    DONE

--- react-first/src/App.jsx
+++ react-first/src/App.jsx
@@ -9,11 +9,14 @@
 // import { DerivedState } from "./components/DerivedState";
 // import { LiftingStateUp } from "./components/LiftingStateUp.jsx";
 import "./components/Ev.css";
-import { ToggleSwitch } from "./src/ToggleSwitch.jsx";
+// import { ToggleSwitch } from "./ToggleSwitch/ToggleSwitch.jsx";
+import { Todo } from "./ToggleSwitch/Todo/Todo";
+
 
 export const App = () => {
   return(
     <section className="container">
+      <Todo />
       {/* <h1 className="card-heading">List of Best Netflix Series</h1>
       {/* <NetflixSeries /> */}
       {/* <EventHandling /> */}
