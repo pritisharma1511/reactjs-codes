227633f322eb5173ec06aef13133febf5f77d68b
pritisharma1511 <ps0130815@gmail.com>
2025-09-04 14:36:22 +0530

    useState done

--- react-first/src/components/hooks/useState/index.jsx
+++ react-first/src/components/hooks/useState/index.jsx
@@ -2,7 +2,7 @@ import "../Hooks.css";
 import { useState } from "react";
 
 export const Counter = () => {
-const[count]
+const[count,setCount] = useState(0);
 
 
     return(
@@ -10,8 +10,8 @@ const[count]
             textAlign:"center" }}>
                 <h1>useState Hook!</h1>
                 <br/>
-                <p> count</p>
-                <button className="state-button">Increment</button>
+                <p> {count}</p>
+                <button className="state-button" onClick={() => setCount(count+1)}>Increment</button>
                 </div>
 
         
