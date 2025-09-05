754f8035b3282b0d7fdcde6f6c0bb076f8832c35
pritisharma1511 <ps0130815@gmail.com>
2025-09-05 11:45:34 +0530

    contactForm created

--- react-first/src/App.jsx
+++ react-first/src/App.jsx
@@ -12,10 +12,10 @@
 // import { ToggleSwitch } from "./ToggleSwitch/ToggleSwitch.jsx";
 //   import { Todo } from "./project/Todo_list/Todo.jsx";
 // import ShortCircuitExample  from "./components/hooks/ShortCircuit.jsx";
-import { LoginForm } from "./components/hooks/useState/LoginForm.jsx";
+import {ContactForm} from "./components/hooks/useState/ContactForm.jsx";
 
 export const App = () => {
-  return <LoginForm/>; 
+  return <ContactForm/>; 
 
     //   {/* <h1 className="card-heading">List of Best Netflix Series</h1>
     //   {/* <NetflixSeries /> */}
