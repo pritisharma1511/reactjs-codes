069da7a118f584b5d3c60fa046ec7578605f2596
pritisharma1511 <ps0130815@gmail.com>
2025-09-06 23:21:58 +0530

    propdrilling done

--- react-first/src/components/PropDrilling.jsx
+++ react-first/src/components/PropDrilling.jsx
@@ -1,5 +1,37 @@
+
 export const ParentComponent = () => {
-    return <h1>Parent Component</h1>;
+  return (
+    <section
+      className={`p-4 h-lvh font-display tracking-wider flex flex-col justify-center  items-center bg-gray-900 text-white`}
+    >
+      <h1>Component A</h1>
+      <ChildComponent data="React JS" />
+    </section>
+  );
 };
 
+const ChildComponent = (props) => {
+  return (
+    <>
+      <h1>Hello, I am component B</h1>
+      <GrandChildComponent data={props.data} />
+    </>
+  );
+};
 
+const GrandChildComponent = (props) => {
+  return (
+    <>
+      <h1>Hello, I am component C</h1>
+      <GrandGrandChildComponent data={props.data} />
+    </>
+  );
+};
+
+const GrandGrandChildComponent = (props) => {
+  return (
+    <>
+      <h1>Hello, I love {props.data}</h1>
+    </>
+  );
+};
