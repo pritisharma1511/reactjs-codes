cc8920e207f6d722bdf22c6fccd3c8793e13fc1b
pritisharma1511 <ps0130815@gmail.com>
2025-09-06 22:31:11 +0530

    useRef comleted

--- react-first/src/App.jsx
+++ react-first/src/App.jsx
@@ -12,10 +12,10 @@
 // import { ToggleSwitch } from "./ToggleSwitch/ToggleSwitch.jsx";
 //   import { Todo } from "./project/Todo_list/Todo.jsx";
 // import ShortCircuitExample  from "./components/hooks/ShortCircuit.jsx";
-import {HowNotToFetchApi} from "./components/hooks/UseEffect/HowNotToFetchApi.jsx";
+import {ForwardRefs} from "./components/hooks/useRefs/ForwardRef.jsx";
 
 export const App = () => {
-  return <HowNotToFetchApi/>; 
+  return <ForwardRefs />;
 
     //   {/* <h1 className="card-heading">List of Best Netflix Series</h1>
     //   {/* <NetflixSeries /> */}
