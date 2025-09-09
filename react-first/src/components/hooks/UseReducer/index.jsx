986b616bdabee127f09986bc3a7d7bd47462f1d3
pritisharma1511 <ps0130815@gmail.com>
2025-09-09 20:30:03 +0530

    done

--- react-first/src/components/hooks/UseReducer/index.jsx
+++ react-first/src/components/hooks/UseReducer/index.jsx
@@ -1,6 +1,14 @@
 import  { use, useReducer } from 'react';
 export const ReducerComp = () => {
-    const Reducer = (state, action) =>{};
+    const Reducer = (state, action) =>{
+        console.log(state, action);
+        if(action.type === 'increment'){
+            return state + 1;
+        }
+        if(action.type === 'decrement'){
+            return state - 1;
+        }   
+    };
     // const [count,setcount] = useState(0);
     const[count, dispatch] = useReducer(Reducer,0);
     console.log(useReducer(Reducer,0));    
@@ -8,9 +16,9 @@ export const ReducerComp = () => {
     return (
         <>
         <div className= "p-4 h-lvh flex flex-col justify-center items-center">
-            <h1></h1>
-             <button>increment</button>
-                <button>decrement</button>  
+            <h1>{count}</h1>
+             <button onClick={() => dispatch({ type: 'increment' })}>increment</button>
+                <button onClick={() => dispatch({ type: 'decrement' })}>decrement</button>
 
         </div>
         </>
