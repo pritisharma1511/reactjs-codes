9215f1824dcee256ca05d226d835da6facd067d8
pritisharma1511 <ps0130815@gmail.com>
2025-09-09 20:22:24 +0530

    done

--- react-first/src/components/hooks/UseReducer/index.jsx
+++ react-first/src/components/hooks/UseReducer/index.jsx
@@ -1,3 +1,23 @@
+import  { useReducer } from 'react';
 export const ReducerComp = () => {
-    return <h1>hello useReducer hook</h1>
+    // const [count,setcount] = useState(0);
+    const[count, dispatch] = useReducer(ReducerComp,0);
+
+    return (
+        <>
+        <div className= "p-4 h-lvh flex flex-col justify-center items-center">
+            <h1></h1>
+             <button>increment</button>
+                <button>decrement</button>  
+
+        </div>
+        </>
+    )
 }
+    
+    
+    
+    
+    
+    
+    
