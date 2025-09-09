5c9b12d8a02cf8a762369b8ae57740d4ce1e84d5
pritisharma1511 <ps0130815@gmail.com>
2025-09-09 20:27:29 +0530

    done

--- react-first/src/components/hooks/UseReducer/index.jsx
+++ react-first/src/components/hooks/UseReducer/index.jsx
@@ -1,7 +1,9 @@
-import  { useReducer } from 'react';
+import  { use, useReducer } from 'react';
 export const ReducerComp = () => {
+    const Reducer = (state, action) =>{};
     // const [count,setcount] = useState(0);
-    const[count, dispatch] = useReducer(ReducerComp,0);
+    const[count, dispatch] = useReducer(Reducer,0);
+    console.log(useReducer(Reducer,0));    
 
     return (
         <>
