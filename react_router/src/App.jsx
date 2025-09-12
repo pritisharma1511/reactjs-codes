1110fac306fceb049bae158716d11daaf45c3a90
pritisharma1511 <ps0130815@gmail.com>
2025-09-12 09:00:22 +0530

    react router

--- react_router/src/App.jsx
+++ react_router/src/App.jsx
@@ -1,28 +1,39 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements,Route, RouterProvider } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Contact } from "./pages/Contact";
 import { Movie } from "./pages/Movie";
 const App = () => {
 
-    const router  = createBrowserRouter([
-        {
-            path: "/",
-            element: <Home />,
-        },
-        {
-          path: "/about",
-          element: <About />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-      {
-        path: "/movie",
-        element: <Movie />,
-      },
-    ]);
+    // const router  = createBrowserRouter([
+    //     {
+    //         path: "/",
+    //         element: <Home />,
+    //     },
+    //     {
+    //       path: "/about",
+    //       element: <About />,
+    //   },
+    //   {
+    //     path: "/contact",
+    //     element: <Contact />,
+    //   },
+    //   {
+    //     path: "/movie",
+    //     element: <Movie />,
+    //   },
+    // ]);
+
+    const router  = createBrowserRouter(
+      createRoutesFromElements(
+        <Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/movie" element={<Movie />} />
+        </Route>
+      )
+    );
 
 
 
