53d815fea8a50402bc58b6161d57b5a43e0ab094
pritisharma1511 <ps0130815@gmail.com>
2025-09-12 09:10:44 +0530

    done

---
+++ react_router/src/components/layout/AppLayout.jsx
@@ -0,0 +1,12 @@
+import { Footer } from "./Footer";  
+import { Header } from "./Header";  
+const AppLayout = () => {
+    return (
+        <>
+        <Header />
+    
+        <Footer />
+        </>
+    );
+};
+export default AppLayout;
