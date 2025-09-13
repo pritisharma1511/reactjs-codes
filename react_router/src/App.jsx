2bdd5fb37a7b5a2af7bc8332c37184f72856bff2
pritisharma1511 <ps0130815@gmail.com>
2025-09-13 23:44:56 +0530

    done

--- react_router/src/App.jsx
+++ react_router/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
       {
         path: "/",
         element: <AppLayout />,
-        errorElement: <ErrorPage />,
+        // errorElement: <ErrorPage />,
         children: [
           {
             path: "/",
