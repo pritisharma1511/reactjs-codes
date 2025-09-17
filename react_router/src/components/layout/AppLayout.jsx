3c442ef73b8ba0bf5ac63136cdedaa3c0697969f
pritisharma1511 <ps0130815@gmail.com>
2025-09-17 17:58:26 +0530

    done

--- react_router/src/components/layout/AppLayout.jsx
+++ react_router/src/components/layout/AppLayout.jsx
@@ -2,6 +2,10 @@ import { Outlet } from "react-router-dom";
 import { Footer } from "./Footer";  
 import { Header } from "./Header";  
 const AppLayout = () => {
+    const navigate = useNavigate();
+    console.log(navigate);
+
+    if(navigate.state === "loading") {return <h1>Loading...</h1>}
     return (
         <>
         <Header />
