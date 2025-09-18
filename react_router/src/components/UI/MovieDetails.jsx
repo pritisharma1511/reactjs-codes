0fbeba0b87502c36f9147e5e7e1bc62d486f2999
pritisharma1511 <ps0130815@gmail.com>
2025-09-18 22:18:44 +0530

    done

--- react_router/src/components/UI/MovieDetails.jsx
+++ react_router/src/components/UI/MovieDetails.jsx
@@ -1,3 +1,7 @@
+import { useParams } from "react-router-dom";
+
 export const MovieDetails = () => {
-  return <h1>Movie Details </h1>
+    const params = useParams();
+    console.log(params);    
+  return <h1>Movie Details for ID: {params.movieId}</h1>
 };
