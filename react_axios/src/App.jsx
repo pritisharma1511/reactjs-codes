5c4faf4665c2d0ad113f97fa4d554fae6049b5e8
pritisharma1511 <ps0130815@gmail.com>
2025-09-19 22:49:44 +0530

    data added

--- react_axios/src/App.jsx
+++ react_axios/src/App.jsx
@@ -1,4 +1,16 @@
+import { getPost } from "./api/PostApi";
+import { useEffect } from "react";
+
 const App = () => {
+  const getPostData = async () => {
+    const res = await getPost();
+    console.log(res.data);
+  };
+  useEffect(() => {
+    getPostData();
+  }, []);
+
+
   return <h1>Hello react curd operation</h1>;
 };
 
