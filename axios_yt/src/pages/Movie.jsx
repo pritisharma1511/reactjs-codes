e89691463238500024407cefdc8ed5ee5340fc05
pritisharma1511 <ps0130815@gmail.com>
2025-09-19 18:33:32 +0530

    done

--- axios_yt/src/pages/Movie.jsx
+++ axios_yt/src/pages/Movie.jsx
@@ -4,19 +4,24 @@ import { Card } from "../components/UI/Card";
 
 export const Movie = () => {
     const[data, setData] = useState([]);
- const API = 
-         "https://www.omdbapi.com/?i=tt3896198&apikey=1c12799f";
-        //  const searchUrl = `https://www.omdbapi.com/?s=titanic&page=1&apikey=1c12799f`;
+//  const API = 
+//          "https://www.omdbapi.com/?i=tt3896198&apikey=1c12799f";
+//         //  const searchUrl = `https://www.omdbapi.com/?s=titanic&page=1&apikey=1c12799f`;
+const API = "https://www.omdbapi.com/?s=titanic&page=1&apikey=1c12799f";
+
 
 
      
     const getMovieData =  async() => {
         try {
         const res = await axios.get(API);
-        console.log(res.data);
-        setData(res.data);
+        console.log(res.data.Search);
+        setData(res.data.Search);
         } catch (error) {
             console.log("error");
+            console.error("Error message:", error.message);
+            console.error("Error status:", error.response.status);
+            console.error("Error data:", error.response.data);
         }   
     };
     useEffect(() => {
@@ -26,11 +31,11 @@ export const Movie = () => {
 
 
 
-  return <ul>
-    {
-       data.map((curElem) =>{
+  return (
+  <ul className = "container.grid.grid-four-cols">
+    {data.map((curElem) =>{
         return <Card key={curElem.imdbID} movieData={curElem} />
-       }) 
-    }
+       })}
   </ul>
-}
+  );    
+};
