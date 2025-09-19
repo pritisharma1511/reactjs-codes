f63df9a3193419073563201f94c1beaaf893763f
pritisharma1511 <ps0130815@gmail.com>
2025-09-19 18:16:02 +0530

    done

--- axios_yt/src/pages/Movie.jsx
+++ axios_yt/src/pages/Movie.jsx
@@ -25,5 +25,11 @@ export const Movie = () => {
 
 
 
-  return <h1>Movie Page</h1>
+  return <ul>
+    {
+       data.map((curElem) =>{
+        return <Card key={curElem.imdbID} movieData={curElem} />
+       }) 
+    }
+  </ul>
 }
