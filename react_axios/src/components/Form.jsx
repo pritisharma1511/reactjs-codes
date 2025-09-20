ef7b52888bec5c0ac36665aa077facf977182e31
pritisharma1511 <ps0130815@gmail.com>
2025-09-20 11:05:26 +0530

    add data added

--- react_axios/src/components/Form.jsx
+++ react_axios/src/components/Form.jsx
@@ -1,7 +1,81 @@
-export const Form = () => {
-    return (
+/* eslint-disable react/prop-types */
+/* eslint-disable no-constant-condition */
+import { useEffect, useState } from "react";
+import { postData, updateData } from "../api/PostApi";
 
-    <form>
+export const Form = ({ data, setData, updateDataApi, setUpdateDataApi }) => {
+  const [addData, setAddData] = useState({
+    title: "",
+    body: "",
+  });
+
+  let isEmpty = Object.keys(updateDataApi).length === 0;
+
+  //   get the udpated Data and add into input field
+  useEffect(() => {
+    updateDataApi &&
+      setAddData({
+        title: updateDataApi.title || "",
+        body: updateDataApi.body || "",
+      });
+  }, [updateDataApi]);
+
+  const handleInputChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setAddData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const addPostData = async () => {
+    const res = await postData(addData);
+    console.log("res", res);
+
+    if (res.status === 201) {
+      setData([...data, res.data]);
+      setAddData({ title: "", body: "" });
+    }
+  };
+
+  //   updatePostData
+  const updatePostData = async () => {
+    try {
+      const res = await updateData(updateDataApi.id, addData);
+      console.log(res);
+
+      if (res.status === 200) {
+        setData((prev) => {
+          return prev.map((curElem) => {
+            return curElem.id === res.data.id ? res.data : curElem;
+          });
+        });
+
+        setAddData({ title: "", body: "" });
+        setUpdateDataApi({});
+      }
+    } catch ({ error }) {
+      console.log(error);
+    }
+  };
+
+  //   form submission
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const action = e.nativeEvent.submitter.value;
+    if (action === "Add") {
+      addPostData();
+    } else if (action === "Edit") {
+      updatePostData();
+    }
+  };
+
+  return (
+    <form onSubmit={handleFormSubmit}>
       <div>
         <label htmlFor="title"></label>
         <input
@@ -10,7 +84,8 @@ export const Form = () => {
           id="title"
           name="title"
           placeholder="Add Title"
-         
+          value={addData.title}
+          onChange={handleInputChange}
         />
       </div>
 
@@ -22,10 +97,13 @@ export const Form = () => {
           placeholder="Add Post"
           id="body"
           name="body"
-         
+          value={addData.body}
+          onChange={handleInputChange}
         />
       </div>
-      <button type="submit" >Add</button>
+      <button type="submit" value={isEmpty ? "Add" : "Edit"}>
+        {isEmpty ? "Add" : "Edit"}
+      </button>
     </form>
   );
 };
--- react_axios/src/components/Posts.jsx
+++ react_axios/src/components/Posts.jsx
@@ -1,58 +1,73 @@
-import { useEffect,useState } from "react";
-import { getPost } from "../api/PostApi";
+
+import { useEffect, useState } from "react";
+import { deletePost, getPost } from "../api/PostApi";
 import "../App.css";
-import  {Form}  from "./Form";
+import { Form } from "./Form";
 
 export const Posts = () => {
-   const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
+  const [updateDataApi, setUpdateDataApi] = useState({});
 
-   const getPostData = async () => {
-        const res = await getPost();
-        console.log(res.data);
-        setData(res.data);
-      };
-      useEffect(() => {
-        getPostData();
-      }, []);
-        const handleDeletePost = async (id) => {
-            try{
-                const res = await deletePost(id);
-                if(res.status === 200) {
-                    const newUpdatedPosts = data.filter((curElem) => {
-                        return curElem.id !== id;
-                    });
-                    setData(newUpdatedPosts);
-                }
-            }catch(err){
-                console.log(err);
-            }
+  const getPostData = async () => {
+    const res = await getPost();
+    console.log(res.data);
+    setData(res.data);
+  };
 
-        };
+  useEffect(() => {
+    getPostData();
+  }, []);
 
-      return (
-        <>
-        <section className="section-form">
-            <Form data={data} setData={setData} />
+  //   function to delete Post
+  const handleDeletePost = async (id) => {
+    try {
+      const res = await deletePost(id);
+      if (res.status === 200) {
+        const newUpdatedPosts = data.filter((curPost) => {
+          return curPost.id !== id;
+        });
+        setData(newUpdatedPosts);
+      } else {
+        console.log("Failed to delete the post:", res.status);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-        </section>
+  //handleUpdatePost
+  const handleUpdatePost = (curElem) => setUpdateDataApi(curElem);
+
+  return (
+    <>
+      <section className="section-form">
+        <Form
+          data={data}
+          setData={setData}
+          updateDataApi={updateDataApi}
+          setUpdateDataApi={setUpdateDataApi}
+        />
+      </section>
       <section className="section-post">
         <ol>
-            {
-                data.map((curElem) => {
-                    const { id, body, title } = curElem;
-                    return <li key={id}>
-                        <p>Title: {title}</p>
-                        <p>Body: {body}</p>
-                        <button>Edit</button>
-                        <button className="btn-delete"
-                         onClick={() => handleDeletePost(id)  }>Delete</button>
-                    </li>
-                })
-            }
+          {data.map((curElem) => {
+            const { id, body, title } = curElem;
+            return (
+              <li key={id}>
+                <p>Title: {title}</p>
+                <p>Body: {body}</p>
+                <button onClick={() => handleUpdatePost(curElem)}>Edit</button>
+                <button
+                  className="btn-delete"
+                  onClick={() => handleDeletePost(id)}
+                >
+                  Delete
+                </button>
+              </li>
+            );
+          })}
         </ol>
       </section>
-      
-      </>
-      );
-     
+    </>
+  );
 };
