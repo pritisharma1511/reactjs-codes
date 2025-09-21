309afd1559d902e42c86160ed476f2c227286460
pritisharma1511 <ps0130815@gmail.com>
2025-09-21 09:59:35 +0530

    Accordian done

--- react_accordian/src/components/Accordian.jsx
+++ react_accordian/src/components/Accordian.jsx
@@ -1,4 +1,38 @@
- export const Accordian = () => {
-    return <h1> The Accordian Component</h1>
-};
+import faq from "../api/faq.json";
+import { useEffect, useState } from "react";
+import { FAQ } from "../components/UI/FAQ.JSX";
 
+export const Accordian = () => {
+  const [data, setData] = useState([]);
+  const [activeId, setActiveId] = useState(false); // Track active FAQ by ID
+
+  useEffect(() => {
+    setData(faq);
+  }, []);
+
+  console.log(data);
+
+  const handleToggle = (id) => {
+    setActiveId((prevId) => (prevId === id ? false : id));
+  };
+
+  return (
+    <>
+      <h1>The Accordion</h1>
+      <ul className="section-accordion">
+        {data &&
+          data.map((curElem) => {
+            const { id } = curElem;
+            return (
+              <FAQ
+                key={id}
+                curData={curElem}
+                isActive={activeId === id}
+                onToggle={() => handleToggle(id)}
+              />
+            );
+          })}
+      </ul>
+    </>
+  );
+};
